ac9b2d1527e57635e478fbfc563c14741373649b
Michele Massaro <m.massaro@zero12.it>
2018-06-13 08:30:01 +0200

    Initial Commit

---
+++ zero12-gaming/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zero12-gaming
+//
+//  Created by Michele Massaro on 13/06/2018.
+//  Copyright © 2018 zero12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
